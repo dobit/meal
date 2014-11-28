62fe7b925e3a045973b972b8ea1efa7659ab68a1
dobit <dobit@163.com>
2014-11-28 17:27:11 +0800

    添加项目

---
+++ LFNet.Meal.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LFNet.Meal.Web.MvcApplication" Language="C#" %>
---
+++ LFNet.Meal.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LFNet.Meal.Web.Startup))]
+namespace LFNet.Meal.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
